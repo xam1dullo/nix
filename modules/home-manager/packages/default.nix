a233e694f37f80cc9fb4f7136851ce06258b27fd
khamidullo <khkhamidullo@gmail.com>
2024-10-16 11:15:23 +0500

    add some vide package

--- modules/home-manager/packages/default.nix
+++ modules/home-manager/packages/default.nix
@@ -89,6 +89,21 @@ in
     nest-cli
     wezterm
 
+
+    ffmpeg
+    # Video/Audio data composition framework tools like "gst-inspect", "gst-launch" ...
+    gst_all_1.gstreamer
+    # Common plugins like "filesrc" to combine within e.g. gst-launch
+    gst_all_1.gst-plugins-base
+    # Specialized plugins separated by quality
+    gst_all_1.gst-plugins-good
+    gst_all_1.gst-plugins-bad
+    gst_all_1.gst-plugins-ugly
+    # Plugins to reuse ffmpeg to play almost every video format
+    gst_all_1.gst-libav
+    # Support the Video Audio (Hardware) Acceleration API
+    gst_all_1.gst-vaapi
+
   ]) ++ (with unstablePkgs; [
     # Add your unstable packages here
     zed-editor
