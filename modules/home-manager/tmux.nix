a874a98751b5f45315f2e8826b779eb3a4863a1f
khamidullo <khkhamidullo@gmail.com>
2025-12-20 22:01:49 +0500

    refactor tmux configuration for consistency; replace single quotes with double quotes in status and window status settings

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -46,14 +46,14 @@ _: {
       set -g status-position top
       set -g status-bg colour234
       set -g status-fg colour137
-      set -g status-left ''
-      set -g status-right '#[fg=colour233,bg=colour241,bold] %d/%m #[fg=colour233,bg=colour245,bold] %H:%M:%S '
+      set -g status-left ""
+      set -g status-right "#[fg=colour233,bg=colour241,bold] %d/%m #[fg=colour233,bg=colour245,bold] %H:%M:%S "
       set -g status-right-length 50
       set -g status-left-length 20
 
       # Window status
-      setw -g window-status-current-format ' #I#[fg=colour250]:#[fg=colour255]#W#[fg=colour50]#F '
-      setw -g window-status-format ' #I#[fg=colour237]:#[fg=colour250]#W#[fg=colour244]#F '
+      setw -g window-status-current-format " #I#[fg=colour250]:#[fg=colour255]#W#[fg=colour50]#F "
+      setw -g window-status-format " #I#[fg=colour237]:#[fg=colour250]#W#[fg=colour244]#F "
 
       # Pane border
       set -g pane-border-style fg=colour238
