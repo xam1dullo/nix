5e08344e36fe0316efea2561e634dc1a82cd2298
khamidullo <khkhamidullo@gmail.com>
2025-12-21 08:19:14 +0500

    update git and nvim problem

--- modules/astronvim.nix/lua/plugins/treesitter.lua
+++ modules/astronvim.nix/lua/plugins/treesitter.lua
@@ -2,13 +2,20 @@
 
 ---@type LazySpec
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = function(_, opts)
-    -- add more things to the ensure_installed table protecting against community packs modifying it
-    opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-      "lua",
-      "vim",
-      -- add more arguments for adding more treesitter parsers
-    })
-  end,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- add more things to the ensure_installed table protecting against community packs modifying it
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+        "lua",
+        "vim",
+        -- add more arguments for adding more treesitter parsers
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    event = "VeryLazy",
+  },
 }
